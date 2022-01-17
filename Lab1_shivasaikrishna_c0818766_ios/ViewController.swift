8554366f37d981b56a1145c8d91cece41eb37a29
ShivaSaiKrishna-c0818766 <c0818766@mylambton.ca>
2022-01-17 17:54:44 -0500

    Initial Commit

---
+++ Lab1_shivasaikrishna_c0818766_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1_shivasaikrishna_c0818766_ios
+//
+//  Created by user204499 on 1/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
